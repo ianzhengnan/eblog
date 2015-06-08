a945ab7bb502f0c5f46d32e82b1ca516d426a768
zhengna <zhengna@zhengnadeMac.local>
2015-06-08 03:00:08 -0700

    update project

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,23 @@
 $(document).ready(function(){
-	$(".blog-nav-item").on('click', function(){
 
+	var active_id = $.cookie("active_id");
+	var id = $.cookie("id");
+
+	if(active_id == undefined && id == undefined){
+		active_id = id = "home";
+	}
+
+	$("#" + active_id).removeClass("active");
+	$("#" + id).addClass("active");
+
+
+	$(".blog-nav-item").on('click', function(){
+		$.cookie("active_id", $(".blog-nav-item.active")[0].id, {
+			expires: 30
+		});
+		$.cookie("id", $(this)[0].id, {
+			expires: 30
+		});
 	});
 
 	$("#search").bind('keypress', function(e){
